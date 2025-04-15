a39344bdc069e91a1fd7d73a73e272ed431f317b
mapilsey <mariiapilina@yandex.com>
2025-04-15 18:10:27 +0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+//изменение для конфликта
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, world from " << name << "!";
+    return 0;
+
+}
