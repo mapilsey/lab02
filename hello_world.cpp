eaa808e1a8ef6775718337ffc0ad7ef4ded3406c
mapilsey <mariiapilina@yandex.com>
2025-04-19 22:35:23 +0300

    Ready to merge

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+<<<<<<< HEAD
 
 // change comment (part 3, point 4)
 
@@ -10,4 +11,19 @@ main() {
   cin >> name;
   cout << "Hello, world from " << name << "!";
   return 0;
+=======
+ 
+<<<<<<< HEAD
+using namespace std;
+
+// change comment (part 3, point 4)
+=======
+>>>>>>> 9bdc626 (Remove 'using namespace std' and qualify all std members explicitly)
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, world from " << name << "!";
+    return 0;
+>>>>>>> 9e30779 ('Remove 'using namespace std' and qualify all std members explicitly)
 }
