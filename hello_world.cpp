796d13ee2c3aa26ca7e1c15b5e0cc11d7cf070a4
mapilsey <mariiapilina@yandex.com>
2025-04-20 11:35:12 +0300

    Update comment Part3 ->Point 4

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 
+// change comment (part 3, point 4)
+
 int
-main()
-{
-  std::string name;
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  std::cout << "Hello, world from " << name << "!";
+main() {
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello, world from " << name << "!";
   return 0;
 }
